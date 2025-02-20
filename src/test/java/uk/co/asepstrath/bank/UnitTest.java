0d2868561b17e7800fdb3ebe5548359b25395c38
fqb23140 <ruairidh.blair.2023@uni.strath.ac.uk>
2025-02-20 22:34:16 +0000

    Added mockito to maven build and working correctly attaching to JVM

--- src/test/java/uk/co/asepstrath/bank/UnitTest.java
+++ src/test/java/uk/co/asepstrath/bank/UnitTest.java
@@ -1,10 +1,44 @@
 package uk.co.asepstrath.bank;
 
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import uk.co.asepstrath.bank.api.AccountAPIParser;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+
+
+
+import org.mockito.Mockito.*;
 
 public class UnitTest {
     /*
     Unit tests should be here
     Example can be found in example/UnitTest.java
      */
+
+    @Test
+    public void AccountApiParserTest(){
+        Logger log = mock(Logger.class);
+        AccountAPIParser test = new AccountAPIParser(log,"test","test");
+        assertNotNull(test);
+    }
+
+    @Test
+    public void AccountApiManipulatorTest(){
+
+    }
+
+    @Test
+    public void AppTests(){
+
+    }
+
+    @Test
+    public void AccountControllerTests(){
+
+    }
+
+
+
 }
