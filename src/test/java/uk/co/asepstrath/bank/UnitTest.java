c2acb9d4d398d79a3ee3447c98b20928e4b2f91c
fqb23140 <ruairidh.blair.2023@uni.strath.ac.uk>
2025-02-21 12:10:57 +0000

    Added unit tests for AccountApiParser and AccountApiManipulator.

--- src/test/java/uk/co/asepstrath/bank/UnitTest.java
+++ src/test/java/uk/co/asepstrath/bank/UnitTest.java
@@ -2,14 +2,16 @@ package uk.co.asepstrath.bank;
 
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
+import uk.co.asepstrath.bank.api.AccountAPIManipulator;
 import uk.co.asepstrath.bank.api.AccountAPIParser;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.mock;
-
-
-
-import org.mockito.Mockito.*;
 
 public class UnitTest {
     /*
@@ -26,7 +28,12 @@ public class UnitTest {
 
     @Test
     public void AccountApiManipulatorTest(){
-
+        Logger log = mock(Logger.class);
+        AccountAPIManipulator testManipulator = new AccountAPIManipulator(log, "src/test/java/uk/co/asepstrath/bank/testJson.json");
+        ArrayList<Account> testList = testManipulator.jsonToAccounts();
+        assertEquals("\"Miss Lavina Waelchi\"",testList.get(0).getName());
+        Map<String, Object> map = testManipulator.createHandleBarsJSONMap();
+        assertEquals("[[{round=No, name=Miss Lavina Waelchi, bal=544.91, uuid=c9dfe369-c5f8-44fd-b9e2-f4fc5ac56ac2}]]",map.get("account_chunks").toString());
     }
 
     @Test
