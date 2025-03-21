3405d94a6a8045ada369dda1d07e5055ccb9caf4
Huda Sudi <huda.abdi-dahir-sudi.2023@uni.strath.ac.uk>
2025-03-21 11:18:41 +0000

    new Html,css, js files

---
+++ src/main/resources/frontend/current/Settings.js
@@ -0,0 +1,7 @@
+function confirmLogout() {
+  const confirmAction = confirm("Are you sure you want to log out?");
+  if (confirmAction) {
+    window.location.href = "LoginPage.html";
+  } 
+  // else do nothing if they cancel
+}
